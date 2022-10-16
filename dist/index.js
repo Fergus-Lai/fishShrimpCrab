2e281c962712083728bdf79e4368b00e910bd6dc
Fergus Lai <ferguslai724@gmail.com>
2022-10-16 23:39:12 +0100

    Create user post function

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const client_1 = require("@prisma/client");
 dotenv_1.default.config();
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get('/', (req, res) => {
-    res.send('Test');
-});
+app.use(express_1.default.json());
+app.post('/user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let { userName, email, password } = req.body;
+    let money = 1000;
+    const post = yield prisma.user.create({
+        data: {
+            userName,
+            email,
+            password,
+            money,
+        }
+    });
+    res.json(post);
+}));
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client'
 
 dotenv.config();
 
+const prisma = new PrismaClient()
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use(express.json());
+
+app.post('/user', async (req: Request, res:Response) => {
+    let {userName, email, password} = req.body;
+    let money = 1000;
+    const post = await prisma.user.create({
+        data: {
+            userName,
+            email,
+            password,
+            money,
+        }
+    })
+    res.json(post)
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
